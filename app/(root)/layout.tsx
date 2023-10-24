9967f873d11f320eb550db6187d336768fcd6211
Farshad Hatami <farshad.hatami95@gmail.com>
2023-10-25 01:58:23 +0330

    feat: right sidebar done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Header />
-          <main>
+          <main className="flex">
             <LeftSidebar />
             <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
               <div className="w-full max-w-4xl">{children}</div>
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -18,6 +19,7 @@ export default function Header() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
