4fa1fceb3afb1fc5c8788a6c6cfb4c708a90d472
Farshad Hatami <farshad.hatami95@gmail.com>
2023-10-26 14:32:11 +0330

    refactor: create form field component

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -24,6 +23,10 @@ type Props = {
   buttonTitle: string;
 };
 
+type FormFieldProps = {
+  fieldName: "name" | "username" | "bio";
+};
+
 export default function AccountProfile({ userData, buttonTitle }: Props) {
   const form = useForm<z.infer<typeof userFormSchema>>({
     resolver: zodResolver(userFormSchema),
@@ -34,6 +37,36 @@ export default function AccountProfile({ userData, buttonTitle }: Props) {
       bio: "",
     },
   });
+
+  const CustomFormField = ({ fieldName }: FormFieldProps) => (
+    <FormField
+      control={form.control}
+      name={fieldName}
+      render={({ field }) => (
+        <FormItem className="flex w-full flex-col items-start gap-2">
+          <FormLabel className="text-base-semibold capitalize text-light-2">
+            {fieldName}
+          </FormLabel>
+          <FormControl className="flex-1 text-base-semibold text-gray-200">
+            {fieldName === "bio" ? (
+              <Textarea
+                rows={10}
+                className="border border-dark-4 bg-dark-3 text-light-1 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+                {...field}
+              />
+            ) : (
+              <Input
+                type="text"
+                className="border border-dark-4 bg-dark-3 text-light-1 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+                {...field}
+              />
+            )}
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 
   const handleImage = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -91,68 +124,13 @@ export default function AccountProfile({ userData, buttonTitle }: Props) {
           )}
         />
         {/* End of the Profile Picture field */}
-        {/* Start of the Name field */}
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem className="flex w-full items-center gap-3">
-              <FormLabel className="text-base-semibold text-light-2">
-                Name
-              </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Input
-                  type="text"
-                  className="border border-dark-4 bg-dark-3 text-light-1 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        {/* End of the Name field */}
-        {/* Start of the Username field */}
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem className="flex w-full items-center gap-3">
-              <FormLabel className="text-base-semibold text-light-2">
-                Username
-              </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Input
-                  type="text"
-                  className="border border-dark-4 bg-dark-3 text-light-1 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        {/* End of the Username field */}
-        {/* Start of the Bio field */}
-        <FormField
-          control={form.control}
-          name="bio"
-          render={({ field }) => (
-            <FormItem className="flex w-full items-center gap-3">
-              <FormLabel className="text-base-semibold text-light-2">
-                Bio
-              </FormLabel>
-              <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Textarea
-                  rows={10}
-                  className="border border-dark-4 bg-dark-3 text-light-1 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        {/* End of the Bio field */}
+        <CustomFormField fieldName="name" />
+        <CustomFormField fieldName="username" />
+        <CustomFormField fieldName="bio" />
 
-        <Button type="submit">{buttonTitle}</Button>
+        <Button type="submit" className="bg-primary-500">
+          {buttonTitle}
+        </Button>
       </form>
     </Form>
   );
